0125548e21c5e8cb161c8745189b21cfd652741e
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2026-08-27 15:31:40 +0800

    feat: add favicon.ico and favicon.svg

--- app/root.tsx
+++ app/root.tsx
@@ -66,6 +66,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
         <script
           dangerouslySetInnerHTML={{ __html: themeInitializationScript }}
         />
